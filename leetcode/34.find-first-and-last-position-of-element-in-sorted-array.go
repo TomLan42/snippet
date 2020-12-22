4aad8ead8efa7a4487dd1464dd22664fda237ce9
TomLan42 <lhc980616@gmail.com>
2020-12-23 01:11:54 +0800

    add files

--- leetcode/34.find-first-and-last-position-of-element-in-sorted-array.go
+++ leetcode/34.find-first-and-last-position-of-element-in-sorted-array.go
@@ -7,6 +7,43 @@
 // @lc code=start
 func searchRange(nums []int, target int) []int {
 
+	ans := []int{-1, -1}
+
+	if len(nums) == 0 {
+		return ans
+	}
+
+	// Find left most
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] >= target {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	if nums[left] == target {
+		ans[0] = left
+	}
+
+	// Find right most
+	left = 0
+	right = len(nums) - 1
+	for left < right {
+		mid := (left + right + 1) / 2
+		if nums[mid] <= target {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	if nums[left] == target {
+		ans[1] = left
+	}
+
+	return ans
 }
 
 // @lc code=end

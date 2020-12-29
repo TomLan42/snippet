5002e75023946a9e93f719da87a2248ab77514de
TomLan42 <lhc980616@gmail.com>
2020-12-29 21:20:32 +0800

    add file

---
+++ leetcode/701.insert-into-a-binary-search-tree.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=701 lang=golang
+ *
+ * [701] Insert into a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	if val > root.Val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+	return root
+}
+
+// @lc code=end
+

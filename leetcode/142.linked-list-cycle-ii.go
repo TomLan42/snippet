d03841553c305acc670534d42a48ffc2977bd1c7
TomLan42 <lhc980616@gmail.com>
2021-01-03 17:39:04 +0800

    add files

---
+++ leetcode/142.linked-list-cycle-ii.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=142 lang=golang
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
+
+// @lc code=end
+

8bde10704bfc338509054a28b7a7b89c990bcb09
TomLan42 <lhc980616@gmail.com>
2021-01-04 22:06:50 +0800

    add files

---
+++ leetcode/234.palindrome-linked-list.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=234 lang=golang
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+    
+}
+// @lc code=end
+

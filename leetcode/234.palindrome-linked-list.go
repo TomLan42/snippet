45ec567714ee3d37d15379e821e7d618903ccf91
Haochong.Lan <lhc980616@gmail.com>
2021-01-04 18:24:35 +0800

    add file

---
+++ leetcode/234.palindrome-linked-list.go
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=234 lang=golang
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	if head.Next == nil {
+		return true
+	}
+
+	//find middle
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next == nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	// split & reverse
+	middle := slow
+	tail := reverseList(middle.Next)
+	middle.Next = nil
+
+	//compare
+	for head != nil && tail != nil {
+		if head.Val == tail.Val {
+			return false
+		}
+		head = head.Next
+		tail = tail.Next
+	}
+	return true
+
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	if head.Next == nil {
+		return head
+	}
+	last := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
+
+// @lc code=end
+

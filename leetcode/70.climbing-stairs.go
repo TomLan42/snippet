e98c6a1d9d3ae0e87a721f6c5903f73dcd13ecbf
TomLan42 <lhc980616@gmail.com>
2021-01-25 21:30:10 +0800

    add file

---
+++ leetcode/70.climbing-stairs.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=70 lang=golang
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+func climbStairs(n int) int {
+
+	f := make([]int, n+1)
+	//init
+	f[0] = 1
+	f[1] = 1
+	for i := 2; i <= n; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+	return f[n]
+
+}
+
+// @lc code=end
+

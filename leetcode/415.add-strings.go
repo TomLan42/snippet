3d2b4d7ab9abb77fb448ff995a78daac5189576e
TomLan42 <lhc980616@gmail.com>
2023-06-18 20:52:46 +0800

    practice

--- leetcode/415.add-strings.go
+++ leetcode/415.add-strings.go
@@ -7,50 +7,49 @@
 // @lc code=start
 func addStrings(num1 string, num2 string) string {
 
-	// make sure num1 is the shorter string
-	if len(num1) > len(num2) {
-		return addStrings(num2, num1)
+	// make sure num1 is the longer string
+	if len(num1) < len(num2) {
+		num1, num2 = num2, num1
 	}
 
-	result := make([]byte, 0)
+	a := toIntSlice(num1)
+	b := toIntSlice(num2)
 
-	carryDigit := 0
-	for i := 0; i < len(num1); i++ {
-
-		num1Digit := int(num1[len(num1)-1-i] - byte('0'))
-		num2Digit := int(num2[len(num2)-1-i] - byte('0'))
-
-		resultDigit := num1Digit + num2Digit + carryDigit
-		carryDigit = resultDigit / 10
-		resultDigit = resultDigit - carryDigit*10
-
-		result = append(result, byte(resultDigit)+byte('0'))
+	carry := 0
+	for i := 0; i < len(a); i++ {
+		if i < len(b) {
+			a[i] += b[i] + carry
+		} else {
+			a[i] += carry
+		}
+		carry = a[i] / 10
+		a[i] %= 10
 	}
 
-	for i := 0; i < len(num2)-len(num1); i++ {
-
-		num2Digit := int(num2[len(num2)-len(num1)-1-i] - byte('0'))
-
-		resultDigit := num2Digit + carryDigit
-		carryDigit = resultDigit / 10
-		resultDigit = resultDigit - carryDigit*10
-
-		result = append(result, byte(resultDigit)+byte('0'))
+	if carry > 0 {
+		a = append(a, carry)
 	}
 
-	if carryDigit != 0 {
-		result = append(result, byte(carryDigit)+byte('0'))
+	return toString(a)
+}
 
+// to int string and reversed
+func toIntSlice(num string) []int {
+	result := make([]int, 0)
+	for i := len(num) - 1; i >= 0; i-- {
+		result = append(result, int(num[i]-'0'))
+	}
+	return result
+}
+
+func toString(nums []int) string {
+	result := ""
+
+	for i := range nums {
+		result += string(nums[len(nums)-1-i] + '0')
 	}
 
-	realResult := make([]byte, 0)
-
-	for i := len(result) - 1; i >= 0; i-- {
-		realResult = append(realResult, result[i])
-	}
-
-	return string(realResult)
-
+	return result
 }
 
 // @lc code=end
